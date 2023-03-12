2c1b6b71f4c74aa1487bc8c9505a53b5c08848a3
Mudit Choudhary <cmudit60@gmail.com>
2023-03-12 19:36:03 +0530

    Feat: Add functions to handle input fields.
    Add different function to handle different input fields
    value.

--- src/Components/EducationDetails.js
+++ src/Components/EducationDetails.js
@@ -1,56 +1,24 @@
 import React from "react";
-
+import EducationInputForm from "./EducationInputForm";
 class EducationDetails extends React.Component {
 	render() {
 		return (
 			<div className="education-details">
 				<h2>Education</h2>
 				<hr />
-				<div className="input-form">
-					<div className="top">
-						<input
-							type="text"
-							id="school-name"
-							name="school-name"
-							value={this.props.detail.schoolName}
-							required
+				{this.props.details.map((detail) => {
+					return (
+						<EducationInputForm
+							detail={detail}
+							key={detail.uniqueId}
+							changeSchoolName={this.props.changeSchoolName}
+                            changeSchoolLocation={this.props.changeSchoolLocation}
+                            changeCourseName={this.props.changeCourseName}
+                            changeCourseStart={this.props.changeCourseStart}
+                            changeCourseEnd={this.props.changeCourseEnd}
 						/>
-
-						<input
-							type="text"
-							id="school-location"
-							name="school-location"
-							value={this.props.detail.schoolLocation}
-							required
-						/>
-					</div>
-					<div className="mid">
-						<input
-							type="text"
-							id="course"
-							name="course"
-							value={this.props.detail.courseName}
-							required
-						/>
-
-						<input
-							type="date"
-							id="school-start"
-							name="school-start"
-							value={this.props.detail.courseStart}
-							required
-						/>
-					</div>
-					<div className="bottom">
-						<input
-							type="date"
-							id="school-end"
-							name="school-end"
-							value={this.props.detail.courseEnd}
-							required
-						/>
-					</div>
-				</div>
+					);
+				})}
 			</div>
 		);
 	}
---
+++ src/Handles/EducationDetailsHandle.js
@@ -0,0 +1,97 @@
+function changeSchoolName(e) {
+	// First get the parent of the input field
+	// but not the parent which has the unique ID
+	let uniqueId = e.target.parentElement;
+	uniqueId = uniqueId.parentElement.id;
+
+	this.setState((prevState) => ({
+		details: {
+			...prevState.details,
+			education: prevState.details.education.map((obj) =>
+				obj.uniqueId === uniqueId
+					? { ...obj, schoolName: e.target.value }
+					: obj
+			),
+		},
+	}));
+}
+
+function changeSchoolLocation(e) {
+	// First get the parent of the input field
+	// but not the parent which has the unique ID
+	let uniqueId = e.target.parentElement;
+	uniqueId = uniqueId.parentElement.id;
+
+	this.setState((prevState) => ({
+		details: {
+			...prevState.details,
+			education: prevState.details.education.map((obj) =>
+				obj.uniqueId === uniqueId
+					? { ...obj, schoolLocation: e.target.value }
+					: obj
+			),
+		},
+	}));
+}
+
+function changeCourseName(e) {
+	// First get the parent of the input field
+	// but not the parent which has the unique ID
+	let uniqueId = e.target.parentElement;
+	uniqueId = uniqueId.parentElement.id;
+
+	this.setState((prevState) => ({
+		details: {
+			...prevState.details,
+			education: prevState.details.education.map((obj) =>
+				obj.uniqueId === uniqueId
+					? { ...obj, courseName: e.target.value }
+					: obj
+			),
+		},
+	}));
+}
+
+function changeCourseStart(e) {
+	// First get the parent of the input field
+	// but not the parent which has the unique ID
+	let uniqueId = e.target.parentElement;
+	uniqueId = uniqueId.parentElement.id;
+
+	this.setState((prevState) => ({
+		details: {
+			...prevState.details,
+			education: prevState.details.education.map((obj) =>
+				obj.uniqueId === uniqueId
+					? { ...obj, courseStart: e.target.value }
+					: obj
+			),
+		},
+	}));
+}
+
+function changeCourseEnd(e) {
+	// First get the parent of the input field
+	// but not the parent which has the unique ID
+	let uniqueId = e.target.parentElement;
+	uniqueId = uniqueId.parentElement.id;
+
+	this.setState((prevState) => ({
+		details: {
+			...prevState.details,
+			education: prevState.details.education.map((obj) =>
+				obj.uniqueId === uniqueId
+					? { ...obj, courseEnd: e.target.value }
+					: obj
+			),
+		},
+	}));
+}
+
+export {
+	changeSchoolName,
+	changeSchoolLocation,
+	changeCourseName,
+	changeCourseStart,
+	changeCourseEnd,
+};

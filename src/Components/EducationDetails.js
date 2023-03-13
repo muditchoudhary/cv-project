2cc85de8503048b735ad874e835bcf57d982ab4d
Mudit Choudhary <cmudit60@gmail.com>
2023-03-13 08:58:34 +0530

    Add a comment for future clearification.

--- src/Components/EducationDetails.js
+++ src/Components/EducationDetails.js
@@ -1,5 +1,18 @@
 import React from "react";
 import EducationInputForm from "./EducationInputForm";
+/**
+ * The data is flowing from the parent to the child
+ * component. i.e From App.js --> EducationDetails.js --> EducationInputFrom.js
+ * Using array of objects to store data of education details.
+ * EducationDetails.js gets the array as prop, map over each obj
+ * inside it and pass the obj to EducationInputForm.js in detail prop.
+ * 
+ * For the handles. Handles are created in a different files.
+ * But are imported into App.js from there they passed to
+ * EducationDetails component as a prop. They also get bind with
+ * App.js this. EducationDetails.js again passes the handle
+ * to EducationInputFrom.js as a prop.
+ */
 class EducationDetails extends React.Component {
 	render() {
 		return (

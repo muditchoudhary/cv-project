5e0843dab47418f197e0eadccfc9e191f1fef77f
Mudit Choudhary <cmudit60@gmail.com>
2023-03-14 12:21:32 +0530

    Style: Style the experience component.

--- src/Components/ExperienceInputForm.js
+++ src/Components/ExperienceInputForm.js
@@ -5,54 +5,69 @@ class ExperienceInputForm extends React.Component {
 		return (
 			<div className="input-form" id={this.props.detail.uniqueId}>
 				<div className="top">
-					<input
-						type="text"
-						id="position"
-						name="position"
-						value={this.props.detail.position}
-                        onChange={this.props.changePosition}
-						required
-					/>
-					<input
-						type="date"
-						id="job-start"
-						name="job-start"
-						value={this.props.detail.jobStart}
-                        onChange={this.props.changeJobStart}
-						required
-					/>
+					<div>
+						<label htmlFor="position">Job Position</label>
+						<input
+							type="text"
+							id="position"
+							name="position"
+							value={this.props.detail.position}
+							onChange={this.props.changePosition}
+							required
+						/>
+					</div>
+					<div>
+						<label htmlFor="company-name">Company Name</label>
+						<input
+							type="text"
+							id="company-name"
+							name="company-name"
+							value={this.props.detail.companyName}
+							onChange={this.props.changeCompanyName}
+							required
+						/>
+					</div>
 				</div>
 				<div className="mid">
-					<input
-						type="text"
-						id="company-name"
-						name="company-name"
-						value={this.props.detail.companyName}
-                        onChange={this.props.changeCompanyName}
-						required
-					/>
-					<input
-						type="date"
-						id="job-end"
-						name="job-end"
-						value={this.props.detail.jobEnd}
-                        onChange={this.props.changeJobEnd}
-						required
-					/>
-				</div>
-				<div className="bottom">
-					<input
-						type="text"
-						id="company-location"
-						name="company-location"
-						value={this.props.detail.jobLocation}
-                        onChange={this.props.changeJobLocation}
-						required
-					/>
+					<div>
+						<label htmlFor="company-location">
+							Company Location
+						</label>
+						<input
+							type="text"
+							id="company-location"
+							name="company-location"
+							value={this.props.detail.jobLocation}
+							onChange={this.props.changeJobLocation}
+							required
+						/>
+					</div>
+					<div>
+						<label htmlFor="job-start">Job Start Date</label>
+						<input
+							type="date"
+							id="job-start"
+							name="job-start"
+							value={this.props.detail.jobStart}
+							onChange={this.props.changeJobStart}
+							required
+						/>
+					</div>
+					<div>
+						<label htmlFor="job-end">Job End Date</label>
+						<input
+							type="date"
+							id="job-end"
+							name="job-end"
+							value={this.props.detail.jobEnd}
+							onChange={this.props.changeJobEnd}
+							required
+						/>
+					</div>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default ExperienceInputForm
+export default ExperienceInputForm;

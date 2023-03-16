e889d782c722eae2fa67b38e611dcf3902b43031
Mudit Choudhary <cmudit60@gmail.com>
2023-03-16 10:59:14 +0530

    Feat: Add a utility.js file.
    This file provide the common methods/datas which will use
    in the components.

---
+++ src/Other/utilities.js
@@ -0,0 +1,40 @@
+// This file contains utility methods and data
+
+function fomratDate(startDate, endDate) {
+	const months = {
+		"01": "January",
+		"02": "February",
+		"03": "March",
+		"04": "April",
+		"05": "May",
+		"06": "June",
+		"07": "July",
+		"08": "August",
+		"09": "September",
+		"10": "October",
+		"11": "November",
+		"12": "December",
+	};
+
+	let startYearVal = startDate.split("-")[0];
+	let startMonthVal = startDate.split("-")[1];
+	let startMonthName = months[startMonthVal];
+	startMonthName = startMonthName.slice(0, 3) + ".";
+
+	let endYearVal = endDate.split("-")[0];
+	let endMonthVal = endDate.split("-")[1];
+	let endMonthName = months[endMonthVal];
+	endMonthName = endMonthName.slice(0, 3) + ".";
+
+	return (
+		startMonthName +
+		" " +
+		startYearVal +
+		" - " +
+		endMonthName +
+		" " +
+		endYearVal
+	);
+}
+
+export { fomratDate };

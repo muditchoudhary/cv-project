145c719fa3a2f926ab0ca6de50d585cd9975813a
Mudit Choudhary <cmudit60@gmail.com>
2023-03-18 09:03:04 +0530

    Shift the initial data to utilities.js

--- src/Other/utilities.js
+++ src/Other/utilities.js
@@ -1,4 +1,6 @@
 // This file contains utility methods and data
+import { v4 as uuidv4 } from "uuid";
+
 
 function fomratDate(startDate, endDate) {
 	const months = {
@@ -51,4 +53,48 @@ function formatDescription(description) {
 
 }
 
-export { fomratDate, formatDescription };
+let initialData = {
+    details: {
+        personalDetails: {
+            name: "Kunal Sekhawat",
+            location: "Delhi India",
+            mail: "kunal@gamil.com",
+            phone: "91888899990",
+            socials: {
+                github: {
+                    name: "GitHub",
+                    link: "https://github.com/kunalsekhawat",
+                },
+                linkedin: {
+                    name: "LinkedIn",
+                    link: "https://www.linkedin.com/in/kunal-sekhawat",
+                },
+            },
+        },
+        education: [
+            {
+                schoolName: "IIM Ahemdabad",
+                schoolLocation: "Ahemdabad, India",
+                courseName: "MBA in Finance",
+                courseStart: "2015-09-01",
+                courseEnd: "2018-09-01",
+                uniqueId: uuidv4(),
+            },
+        ],
+
+        experience: [
+            {
+                position: "Product Manager",
+                jobStart: "2018-12-01",
+                companyName: "XYZ",
+                jobEnd: "2019-09-01",
+                jobLocation: "Banglore, India",
+                jobDescription:
+                    "- We deeply believe in creating an inclusive and diverse conference and want to make sure that everyone is able to participate. - We do not want your thirst for knowledge to be limited due to any social or economic hardships. - That is why React India has organized a diversity/scholarship program to help you attend this international event and learn about React from the pros. - We also want to make sure that local students and others residents are able to attend.",
+                uniqueId: uuidv4(),
+            },
+        ],
+    },
+};
+
+export { fomratDate, formatDescription, initialData };
